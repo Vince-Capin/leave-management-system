3c1728496e5691ab04ca08ca85d90c1e32fda29d
gelayyy66 <angela.villaester@synacy.com>
2025-09-12 12:27:22 +0800

    Final Unit test

--- src/test/groovy/com/synacy/trainee/leavemanagementsystem/leaveCredits/LeaveCreditsServiceSpec.groovy
+++ src/test/groovy/com/synacy/trainee/leavemanagementsystem/leaveCredits/LeaveCreditsServiceSpec.groovy
@@ -39,33 +39,4 @@ class LeaveCreditsServiceSpec extends Specification {
         def ex = thrown(LeaveCreditsNotFoundException)
         ex.message == "Leave credits for user 3 not found"
     }
-
-//    def "setLeaveCreditsForNewUsers() should create and save leave credits with correct values"() {
-//        given: "a user and a user request dto"
-//        User user = new User()
-//        def userRequest = Mock(UserRequestDTO) {
-//            leaveCredits() >> 15
-//        }
-//
-//        and: "expected leaveCredits object"
-//        LeaveCredits savedLeaveCredits = new LeaveCredits(
-//                user: user,
-//                totalLeaveCredits: 15,
-//                remainingLeaveCredits: 15
-//        )
-//
-//        when: "the service is called"
-//        leaveCreditsRepository.save(_ as LeaveCredits) >> { LeaveCredits lc -> lc } // return the same object passed
-//        def result = leaveCreditsService.setLeaveCreditsForNewUsers(user, userRequest)
-//
-//        then: "repository save should be called once"
-//        1 * leaveCreditsRepository.save(_ as LeaveCredits)
-//
-//        and: "the result should have correct values"
-//        result.user == user
-//        result.totalLeaveCredits == 15
-//        result.remainingLeaveCredits == 15
-//    }
-//
-
 }
